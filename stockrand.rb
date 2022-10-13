8baffd929bce4c3cfe11ca4b1af59d78266c9d68
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2022-10-13 19:39:59 +0200

    four group model

--- stockrand.rb
+++ stockrand.rb
@@ -2,22 +2,57 @@ require 'json'
 require 'open-uri'
 
 # def variables
-all_stocks = []
-stock_pics = []
+stock_picks_a = []
+stock_picks_b = []
+stock_picks_c = []
+stock_picks_d = []
 
 # scrape all securities available
 url = "https://raw.githubusercontent.com/RayBB/random-stock-picker/master/stocks.json"
-cts = open(url).read
-cts_p = JSON.parse(cts)
+tickers = open(url).read
+tickers_p = JSON.parse(tickers)
 
 # random selection
 20.times do
-  stock_pics << cts_p.sample
+  pick = tickers_p.sample
+  stock_picks_a << pick
+  pick_ind = tickers_p.find_index(pick)
+  tickers_p.delete_at(pick_ind)
+end
+20.times do
+  pick = tickers_p.sample
+  stock_picks_b << pick
+  pick_ind = tickers_p.find_index(pick)
+  tickers_p.delete_at(pick_ind)
+end
+20.times do
+  pick = tickers_p.sample
+  stock_picks_c << pick
+  pick_ind = tickers_p.find_index(pick)
+  tickers_p.delete_at(pick_ind)
+end
+20.times do
+  pick = tickers_p.sample
+  stock_picks_d << pick
+  pick_ind = tickers_p.find_index(pick)
+  tickers_p.delete_at(pick_ind)
 end
 
 # final output
-puts "From stock pics to analyse are:"
-stock_pics.each do |i|
-  puts i
-end
-puts "enjoy!"
+puts "For group a the stock pics to analyse are:"
+puts stock_picks_a
+
+puts ""
+
+puts "For group b the stock pics to analyse are:"
+puts stock_picks_b
+
+puts ""
+
+puts "For group c the stock pics to analyse are:"
+puts stock_picks_c
+
+puts ""
+
+puts "For group d the stock pics to analyse are:"
+puts stock_picks_d
