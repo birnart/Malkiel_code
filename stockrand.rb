276c2c909dc52d0807f2c406f5d1d427f2aa0db7
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2022-10-31 18:14:11 +0100

    done

--- stockrand.rb
+++ stockrand.rb
@@ -1,26 +1,27 @@
 require 'json'
 require 'open-uri'
-# scrape all securities available
-url = "https://raw.githubusercontent.com/RayBB/random-stock-picker/master/stocks.json"
-tickers = open(url).read
-tickers_p = JSON.parse(tickers).shuffle
+# scrape all securities available => FIND A BETTER HASH TO DO META ANAYLSIS
+# url = "https://raw.githubusercontent.com/RayBB/random-stock-picker/master/stocks.json"
+# tickers = open(url).read
+# tickers_p = JSON.parse(tickers).shuffle
+all_tickers = JSON.parse(open("https://raw.githubusercontent.com/RayBB/random-stock-picker/master/stocks.json").read).shuffle
 # input
 puts "how many groups are you working in? (integer input only)"
-gr_num = gets.chomp.to_i
+group_num = gets.chomp.to_i
 puts "how many option do you want per group? (integer input only)"
-pk_num = gets.chomp.to_i
+pick_num = gets.chomp.to_i
 # random selection
 i = 1
-gr_num.times do
-  selec = []
-  pk_num.times do
-    pick = tickers_p.sample
-    selec << pick
-    tickers_p.delete_at(tickers_p.find_index(pick))
+group_num.times do
+  selection = []
+  pick_num.times do
+    pick = all_tickers.sample
+    selection << pick
+    all_tickers.delete_at(all_tickers.find_index(pick))
   end
   # output
   puts ""
   puts "For group #{i} the securities to analyse are:"
-  puts selec
+  puts selection
   i += 1
 end
