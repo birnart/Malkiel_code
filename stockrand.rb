53ae7ec952d63a64503af40b3e5a73e2cbf21c2d
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2022-10-31 11:28:31 +0100

    made number of picks customizable from cosole

--- stockrand.rb
+++ stockrand.rb
@@ -1,30 +1,32 @@
 require 'json'
 require 'open-uri'
 
-# def variables
-stock_picks_a = []
-stock_picks_b = []
-stock_picks_c = []
-stock_picks_d = []
-focus_groups = [stock_picks_a, stock_picks_b, stock_picks_c, stock_picks_d]
-
+focus_groups = [a = [], b = [], c = [], d = []]
+num = "something"
 # scrape all securities available
 url = "https://raw.githubusercontent.com/RayBB/random-stock-picker/master/stocks.json"
 tickers = open(url).read
 tickers_p = JSON.parse(tickers)
 
+
+puts "how many option do you want per group? (integer input only)"
+num = gets.chomp.to_i
+
+
 # random selection
 focus_groups.each do |group|
-  20.times do
+  num.times do
     pick = tickers_p.sample
     group << pick
     pick_ind = tickers_p.find_index(pick)
     tickers_p.delete_at(pick_ind)
   end
 end
+# output
 i = 1
 while i < 5 do
   puts "For group #{i} the stock pics to analyse are:"
   puts focus_groups.pop
+  puts ""
   i += 1
 end
