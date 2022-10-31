f9f0b4ffc6f09cb58b2669018c4fa729ff5df205
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2022-10-31 12:26:38 +0100

    cut down code length significantly and made it more simple and effective

--- stockrand.rb
+++ stockrand.rb
@@ -1,32 +1,26 @@
 require 'json'
 require 'open-uri'
-
-focus_groups = [a = [], b = [], c = [], d = []]
-num = "something"
 # scrape all securities available
 url = "https://raw.githubusercontent.com/RayBB/random-stock-picker/master/stocks.json"
 tickers = open(url).read
-tickers_p = JSON.parse(tickers)
-
-
+tickers_p = JSON.parse(tickers).shuffle
+# input
+puts "how many groups are you working in? (integer input only)"
+gr_num = gets.chomp.to_i
 puts "how many option do you want per group? (integer input only)"
-num = gets.chomp.to_i
-
-
+pk_num = gets.chomp.to_i
 # random selection
-focus_groups.each do |group|
-  num.times do
-    pick = tickers_p.sample
-    group << pick
-    pick_ind = tickers_p.find_index(pick)
-    tickers_p.delete_at(pick_ind)
-  end
-end
-# output
 i = 1
-while i < 5 do
-  puts "For group #{i} the stock pics to analyse are:"
-  puts focus_groups.pop
+gr_num.times do
+  selec = []
+  pk_num.times do
+    pick = tickers_p.sample
+    selec << pick
+    tickers_p.delete_at(tickers_p.find_index(pick))
+  end
+  # output
   puts ""
+  puts "For group #{i} the securities to analyse are:"
+  puts selec
   i += 1
 end
